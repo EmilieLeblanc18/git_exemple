6b8574d358264ed266d56ea5a414c9bcad0ba593
Mlambert4 <Meglambert121@gmail.com>
2026-02-17 10:39:19 -0500

    Trousse de 1er soins

--- exemple.R
+++ exemple.R
@@ -1,3 +1,13 @@
 #exemple de modification
 
 usethis::use_git_config(user.name = "EmilieLeblanc18", user.email = "emilie.leblanc1818@gmail.com")
+
+
+#Dans file -> New project -> Version control -> Git -> copier coller URL
+
+#Pour envoyer 
+#Enregistrer modif -> dans onglet Git -> commit -> select fichier a envoyer -> ecrire un commit message -> commit -> Push 
+
+#Lorsque probleme -> faire revert 
+
+#Pour avoir les nouvelles modifications -> faire Pull 
